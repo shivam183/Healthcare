b6c6fe9b721214f5e9a1205656206c868842179b
shivam183 <soodshivam1831997@gmail.com>
2018-10-21 01:18:30 -0400

    Updated Add Patient to Reactive Forms && Added Validations

--- client/src/app/patients/patients.component.ts
+++ client/src/app/patients/patients.component.ts
@@ -3,12 +3,11 @@ import { PatientService } from '../patient.service';
 import { Patient } from '../Patient';
 import { FlashMessagesService } from 'angular2-flash-messages';
 import { Router } from '@angular/router'
-import { ValidateService } from '../validate.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-patients',
   templateUrl: './patients.component.html',
   styleUrls: ['./patients.component.css'],
-  providers: [PatientService]
 })
 export class PatientsComponent implements OnInit {
 
@@ -26,12 +25,67 @@ export class PatientsComponent implements OnInit {
   department: String;
   doctor: String
 
+  addPatientForm: FormGroup;
+
+
+  formErrors = {
+    'first_name': '',
+    'last_name': '',
+    'DOB': '',
+    'email': '',
+    'address': '',
+    'city': '',
+    'province': '',
+    'postal_code': '',
+    'phone': '',
+    'department': '',
+    'doctor': '',
+  }
+
+  validationMessages = {
+    'first_name': {
+      'required': 'First Name is Required'
+    },
+    'last_name': {
+      'required': 'Last Name is Required'
+    },
+    'DOB': {
+      'required': 'Date Of Birth is Required'
+    },
+    'email': {
+      'required': 'Email is Required',
+      'email': 'Please Provide Correct Email'
+    },
+    'address': {
+      'required': 'Address is Required'
+    },
+    'city': {
+      'required': 'City is Required'
+    },
+    'province': {
+      'required': 'Province is Required'
+    },
+    'postal_code': {
+      'required': 'Postal Code is Required'
+    },
+    'phone': {
+      'required': 'Phone is Required'
+    },
+    'department': {
+      'required': 'Department is Required'
+    },
+    'doctor': {
+      'required': 'Doctors Name is Required'
+    },
+
+  }
+
   constructor
     (
     private patientService: PatientService,
     private flashMessage: FlashMessagesService,
     private router: Router,
-    private validate: ValidateService
+    private fb: FormBuilder
     ) { }
 
 
@@ -53,21 +107,8 @@ export class PatientsComponent implements OnInit {
 
     }
 
-    /* if (!this.validate.validatePatient(newPatient)) {
- 
-       this.flashMessage.show('All Fields are Required', { cssClass: 'alert-danger', timeout: 3000 })
-       return false;
-     }
- 
-     if (!this.validate.validateEmail(newPatient.email)) {
- 
-       this.flashMessage.show('Please enter a valid Email', { cssClass: 'alert-danger', timeout: 3000 })
-       return false;
-     } */
-
     this.patientService.addPatient(newPatient).subscribe((patient: any) => {
-      console.log(patient);
-      //this.patients.push(patient);
+      this.patients = patient;
       this.flashMessage.show("Patient Sucessfully added", { cssClass: 'alert-success', timeout: 3000 });
       this.router.navigate(['/dashboard']);
     });
@@ -79,8 +120,42 @@ export class PatientsComponent implements OnInit {
 
   ngOnInit() {
 
+    this.addPatientForm = this.fb.group({
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      DOB: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      address: ['', Validators.required],
+      city: ['', Validators.required],
+      province: ['', Validators.required],
+      postal_code: ['', Validators.required],
+      phone: ['', Validators.required],
+      department: ['', Validators.required],
+      doctor: ['', Validators.required]
+    });
 
+    this.addPatientForm.valueChanges.subscribe((data) => {
+      this.logValidationErrors(this.addPatientForm);
+    })
+  }
 
+  logValidationErrors(group: FormGroup = this.addPatientForm): void {
+    Object.keys(group.controls).forEach((key: string) => {
+      const abstractControl = group.get(key);
+      if (abstractControl instanceof FormGroup) {
+        this.logValidationErrors(abstractControl);
+      } else {
+        this.formErrors[key] = '';
+        if (abstractControl && !abstractControl.valid && (abstractControl.touched || abstractControl.dirty)) {
+          const messages = this.validationMessages[key];
+          for (const errorKey in abstractControl.errors) {
+            if (errorKey) {
+              this.formErrors[key] += messages[errorKey] + ' ';
+            }
+          }
+        }
+      }
+    })
   }
 
 }
