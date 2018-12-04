3196d62e212a3102344d4130f47b2b75f465ca5e
shivam183 <soodshivam1831997@gmail.com>
2018-12-04 17:12:41 -0500

    Moved database to mongo atlas

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://shivam:shivamnice123@ds237723.mlab.com:37723/healthcare',
+    database: 'mongodb://shivam:nice@healthcare-shard-00-00-i3gh5.mongodb.net:27017,healthcare-shard-00-01-i3gh5.mongodb.net:27017,healthcare-shard-00-02-i3gh5.mongodb.net:27017/test?ssl=true&replicaSet=HealthCare-shard-0&authSource=admin&retryWrites=true',
     secret: "TopSecret"
 }
